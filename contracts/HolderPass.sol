eba9f6d4f378da664d04aa92ea2ed0ae7d05e5ff
norika <user@USERnoMacBook-Pro.local>
2022-10-18 13:02:38 +0900

    Update the HolderPass and add deployHolderPass

--- contracts/HolderPass.sol
+++ contracts/HolderPass.sol
@@ -16,18 +16,23 @@ pragma solidity ^0.8.9;
 
 import { ERC165, IERC165, ERC165Storage }  from "@solidstate/contracts/introspection/ERC165.sol";
 import { IERC1155 } from "@solidstate/contracts/interfaces/IERC1155.sol";
-import { AccessControl } from "@openzeppelin/contracts/access/AccessControl.sol";
-import "@solidstate/contracts/token/ERC1155/SolidStateERC1155.sol";
+import { AccessControl } from "@solidstate/contracts/access/access_control/AccessControl.sol";
+import { EnumerableSet } from "@solidstate/contracts/utils/EnumerableSet.sol";
 import { ERC1155MetadataStorage } from "@solidstate/contracts/token/ERC1155/metadata/ERC1155MetadataStorage.sol";
+import { ERC1155EnumerableStorage } from "@solidstate/contracts/token/ERC1155/enumerable/ERC1155EnumerableStorage.sol";
+import "@solidstate/contracts/token/ERC1155/SolidStateERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract HolderPass is SolidStateERC1155, Ownable {
+contract HolderPass is SolidStateERC1155, AccessControl {
+    using ERC165Storage for ERC165Storage.Layout;
+    using EnumerableSet for EnumerableSet.AddressSet;
+
 
     // Token informations
     string public name;
     string public symbol;
     string public contractURI;
-    using ERC165Storage for ERC165Storage.Layout;
+    uint256 public MAX_NUMBER = 3;
     bytes32 private constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
     constructor(
@@ -48,25 +53,79 @@ contract HolderPass is SolidStateERC1155, Ownable {
         l.baseURI = _contractURI;
         name = _name;
         symbol = _symbol;
-
+        _grantRole(MINTER_ROLE, msg.sender);
     }
 
-    function mint(
+    function mintPass(
         address account,
         uint256 id
-    ) external onlyOwner{
+    ) external onlyRole(MINTER_ROLE)  {
+        if(totalSupply(id) > MAX_NUMBER) {
+            address secondPassHolder = _addrByIndex(id, 1);
+            _burn(secondPassHolder, id, 1); 
+        }
         _mint(account, id, 1, '');
     }
 
     function burn(
         address account,
         uint256 id
-    ) external onlyOwner {
+    ) external onlyRole(MINTER_ROLE) {
         _burn(account, id, 1);
     }
 
-    function setContractURI(string calldata _uri) external onlyOwner {
+    function setContractURI(string calldata _uri) external onlyRole(MINTER_ROLE) {
         contractURI = _uri;
     }
+
+    /**
+     * @notice query idx of account who owns token id
+     * @param id: token id, addr: address
+     * @return index of Account
+     */
+    function indexOfAddr(uint256 id, address addr)
+        external
+        view
+        returns (uint256)
+    {
+        // TODO: if accounts is the list of account, not index.
+        EnumerableSet.AddressSet storage accounts = ERC1155EnumerableStorage
+            .layout()
+            .accountsByToken[id];
+
+        // checker: contains 
+        // value => index
+        require(accounts.contains(addr), "Invalid Index");
+        return accounts.indexOf(addr);
+    }
+
+    function addrByIndex(uint256 id, uint256 index)
+        external
+        view
+        returns (address)   
+    {
+        return _addrByIndex(id, index);
+    }
+
+    /**
+     * @notice query idx of account who owns token id
+     * @param id: token id, addr: address
+     * @return index of Account
+     */
+    function _addrByIndex(uint256 id, uint256 index)
+        internal
+        view
+        returns (address)
+    {
+        EnumerableSet.AddressSet storage accounts = ERC1155EnumerableStorage
+            .layout()
+            .accountsByToken[id];
+
+        // value => index
+        require(accounts.length() > index, "Invalid index");
+        return accounts.at(index);
+    }
+
+
 
 }
